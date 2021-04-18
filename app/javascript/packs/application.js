b768290b396d851a86dec28e82d987f203720ffa
YaroslavTupitskyi <jaroslav.typitsky@gmail.com>
2021-04-18 12:28:25 +0300

    initial commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,16 @@
+require("popper.js/dist/umd/popper.min")
+require("bootstrap/dist/js/bootstrap.bundle.min")
+require("jquery.easing")
+require("waypoints/lib/jquery.waypoints.min")
+require("jquery.counterup")
+require("venobox")
+require("owl.carousel/dist/owl.carousel.min")
+require("aos/dist/aos")
+require("js/application")
+
+// import Rails from "@rails/ujs"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+
+// Rails.start()
+// ActiveStorage.start()
